bf4e3f56c0d9b224e888f63750c22d4b0b180971
alexizumi <alexizumi12@gmail.com>
2025-09-16 15:40:46 +0100

    Setting up NextJS and Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+      },
+    },
+  },
+} satisfies Config;
